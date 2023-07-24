ed5837e60331c512e59dc8dc36b2578d82ef6e7f
brendowcaval <brendow.c@yahoo.com>
2023-07-24 14:07:31 -0300

    segunda versão do sistema

--- README.md
+++ README.md
@@ -1,35 +1,45 @@
-# Desafio : Criando um sistema bancário
+# Desafio : Otimizando o Sistema Bancário
 
 Desafio realizado para o Bootcamp Potência Tech powered by iFood | Ciência de Dados com Python.
 
 ## Objetivo Geral
 
-Criar um sistema bancário com as operações: sacar, depositar e visualizar extrato.
+Separar as funções existentes de saque, depósito e extrato em funções. Criar duas novas funções : cadastrar usuário (cliente) e cadastrar conta bancária.
 
 ## Desafio 
 
-Fomos contratados por um grande banco para desenvolver o seu novo sistema. Esse banco deseja modernizar suas operações e para isso escolheu a linguagem python. Para a primeira versão do sistema devemos implementar apenas 3 operações : depósito, saque e extrato.
+Precisamos deixar nosso código mais modularizado, para isso vamos criar funções para as operações existentes: sacar, depositar e visualizar extrato. Além disso, para a versão 2 do nosso sistema precisamos criar duas novas funções : criar usuário (cliente do banco) e criar conta corrente (vincular com o usuário) 
 
-#### Operação de Depósito
-- Depositar apenas valores positivos para a conta
-- Essa versão trabalha apenas com um usuário, sem se preocupar com número de agência e conta bancária.
-- os depósitos devem ser exibidos na operação de extrato.
+#### Separação em funções
 
-#### Operação de Saque
-- Permitido realizar 3 saques diários
-- Permitido sacar com limite máximo de R$ 500 por saque.
-- Os saques devem ser exibidos na operação de extrato.
+Deve-se criar funções para todas as operações do sistema. Para exercitar tudo o que foi aprendido, cada função vai ter uma regra na passagem de argumentos. O retorno e a forma como serão chamadas, pode ser definida por você da forma que achar melhor.
 
-#### Operação de extrato
-- Essa operação lista todos os depósitos e saques
-- No fim da listagem, é exibido o saldo atual da conta
 
+#### Saque
+
+A função saque deve receber os argumentos apenas por nome (***keyword only***)
+
+#### Depósito 
+
+A função depósito deve receber os argumentos apenas por posição (***positional only***)
+
+#### Extrato 
+
+a função extrato deve receber os argumentos por posição e nome (***positional only e keyword only***). argumentos posicionais : saldo, argumentos nomeados : extrato.
+
+#### Criar Usuário (cliente) 
+
+o programa deve armazenar os usuários em uma lista, um usuário é composto por : nome, data de nascimento, cpf e endereço. Não podemos cadastrar 2 usuários com o mesmo CPF.
+
+#### Criar Conta Corrente 
+
+o programa deve armazenar contas em uma lista, uma conta é composta por : agência, número da conta e usuário. O número da conta é sequencial, começando em 1, e o da agência é fixo : "0001". O usuário pode ter mais de uma conta, mas uma conta pertence somente a um usuário.
 
 
 ## Links
 
 [ Bootcamp Potência Tech powered by iFood | Ciência de Dados com Python](https://web.dio.me/track/potencia-tech-powered-ifood-ciencias-de-dados-com-python)
 
-[Desafio Sistema Bancário](https://web.dio.me/project/desafio-de-projeto-criando-um-sistema-bancario/learning/fa812356-0da6-4a85-9ffb-8b255748a288?back=/track/potencia-tech-powered-ifood-ciencias-de-dados-com-python&tab=undefined&moduleId=undefined)
+[Desafio Otimizando o Sistema Bancário](https://web.dio.me/lab/otimizando-o-sistema-bancario-com-funcoes-python/learning/82a55799-cfb8-479d-85a3-4982e29c90ba)
 
 
--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,3 +1,14 @@
+menu_criacao = """
+
+  [u] Criar Usuário
+  [c] Criar conta corrente
+  [a] Acessar conta
+  [l] Listar Contas
+  [q] Sair
+
+=> """
+
+
 menu = """
 
   [d] Depositar
@@ -12,49 +23,176 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+usuarios_cpf = []
+usuarios_dados = {}
+conta_corrente = {}
+numero_conta = 0
+
+
+def depositar(saldo,extrato,/):
+
+    valor_depositado = int(input("Informe o valor para depositar : "))
+    if valor_depositado < 1:
+        return "operação inválida, apenas é aceitável valores positivos."
+    else:
+        saldo += valor_depositado
+        extrato += f"valor de {valor_depositado:.2f} foi depositado.\n"
+
+        return saldo, extrato
+
+
+def sacar(*, numero_saques, limite_saques, saldo, extrato, limite):
+    if numero_saques == limite_saques:
+        return "Não é possivel mais sacar, o limite é 3 saques diários."
+    else:
+        sacar_valor = int(input("Informe um valor para sacar : "))
+
+        if sacar_valor < 1 :
+            return "operação inválida, só é possivel sacar valores positivos"
+        elif sacar_valor > saldo:
+            return "Não é possivel sacar o dinheiro por falta de saldo na conta."
+        elif sacar_valor > limite:
+            return "operação inválida, limite máximo de valor para sacar é R$ 500"
+        else:
+            saldo -= sacar_valor
+            numero_saques += 1
+            extrato += f"foi realizado um saque de {sacar_valor:.2f}.\n"
+
+            return saldo, numero_saques, extrato
+
+
+
+def visualizar_extrato(saldo, /, *, extrato):
+    if extrato == "":
+        return "Não foram realizadas movimentações."
+    else:
+        return extrato, f"Saldo atual : {saldo:.2f}"
+        
+
+
+def criar_usuário(nome, data_nascimento,cpf, endereco):
+    usuarios_cpf.append(cpf)
+    usuarios_dados[cpf] = {"data-nascimento" : data_nascimento, "nome" : nome, "endereço": endereco }
+    print("usuário cadastrado com sucesso!")
+
+
+
+def verificar_usuario(cpf):
+    
+    if usuarios_cpf.count(cpf) == 1:
+        print("Erro, esse CPF já está cadastrado!")
+    else:
+        criar_usuário(nome,data_de_nascimento,cpf,endereco)
+
+
+     
+
+def criar_conta_corrente(cpf): 
+    # verificar se o CPF está cadastrado
+    if usuarios_cpf.count(cpf) == 1:
+        agencia = "0001"
+        global numero_conta
+        numero_conta += 1
+        conta_corrente[cpf] = {"agência" : agencia, "numero_conta": numero_conta, "usuario": usuarios_dados[cpf]["nome"] }
+        print("Conta corrente criada com sucesso!")
+    else:
+        print("Esse CPF é inválido!")
+
+
+def acessando_conta_corrente(agencia, conta, usuario):
+    
+    while True:
+        print(f"Agência : {agencia} | Conta : {conta} | Usuário : {usuario} ")
+        opcao = input(menu)
+
+        if opcao == "d":
+            retorno_depositar = depositar(saldo,extrato)
+
+            if type(retorno_depositar) == type(str()):
+                print(retorno_depositar)
+            else:
+                saldo = retorno_depositar[0]
+                extrato = retorno_depositar[1]
+            
+
+        elif opcao == "s":    
+
+            retorno_sacar = sacar(numero_saques=numero_saques,
+                                     limite_saques=LIMITE_SAQUES,
+                                     saldo=saldo,
+                                     extrato=extrato,
+                                     limite=limite)
+
+            if type(retorno_sacar) == type(str()):
+                print(retorno_sacar)
+            else:
+                saldo = retorno_sacar[0]
+                numero_saques = retorno_sacar[1] 
+                extrato = retorno_sacar[2]
+        
+        elif opcao == "e":
+
+            retorno_extrato = visualizar_extrato(saldo, extrato= extrato)
+            if retorno_extrato == "Não foram realizadas movimentações.":
+                print(retorno_extrato)
+            else:
+                print(retorno_extrato[0] + "\n" + retorno_extrato[1])
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("operação inválida, por favor selecione novamente a operação desejada!")
 
 
 while True:
 
-    opcao = input(menu)
+    opcao_menu = input(menu_criacao)
 
-    if opcao == "d":
-        valor_depositado = int(input("Informe o valor para depositar : "))
-        if valor_depositado < 1:
-            print("operação inválida, apenas é aceitável valores positivos.")
+    if opcao_menu == "u":
+        nome = input("Informe seu nome : ")
+        data_de_nascimento = input("Informa a data de nascimento : ")
+        cpf = int(input("Informe o CPF(somente números) : "))
+        endereco = input("Informe o endereço (ex : rua - numero - bairro - cidade/sigla estado) : ")
+
+        verificar_usuario(cpf)
+                     
+    elif opcao_menu == "c":
+        cpf = int(input("Informe o CPF(somente números) : "))
+        criar_conta_corrente(cpf)
+
+    elif opcao_menu == "a":
+        cpf = int(input("Informe o CPF(somente números) : "))
+        conta_existe = conta_corrente.get(cpf, False)
+
+        if conta_existe == False:
+            print("Erro, essa conta não existe!")
         else:
-            saldo += valor_depositado
-            extrato += f"valor de {valor_depositado:.2f} foi depositado.\n"
-            
+            arg_1 = conta_corrente[cpf]["agência"]
+            arg_2 = conta_corrente[cpf]["numero_conta"]
+            arg_3 = conta_corrente[cpf]["usuario"]
+            acessando_conta_corrente(arg_1, arg_2, arg_3)
 
-    elif opcao == "s":    
-
-        if numero_saques == LIMITE_SAQUES:
-            print("Não é possivel mais sacar, o limite é 3 saques diários.")
-            
+    elif opcao_menu == "l":
+        if conta_corrente == {}:
+            print("Nenhuma conta foi cadastrada no momento!")
         else:
-            sacar_valor = int(input("Informe um valor para sacar : "))
-
-            if sacar_valor < 1 :
-                print("operação inválida, só é possivel sacar valores positivos")
-            elif sacar_valor > saldo:
-                print("Não é possivel sacar o dinheiro por falta de saldo na conta.")
-            elif sacar_valor > limite:
-                print("operação inválida, limite máximo de valor para sacar é R$ 500")
-            else:
-               saldo -= sacar_valor
-               numero_saques += 1
-               extrato += f"foi realizado um saque de {sacar_valor:.2f}.\n"
-        
-    elif opcao == "e":
-        if extrato == "":
-            print("Não foram realizadas movimentações.")
-        else:
-            print(extrato)
-            print(f"Saldo atual : {saldo:.2f}")
-
-    elif opcao == "q":
+            for chave in conta_corrente:
+                print(f""" 
+                Agência : {conta_corrente[chave]["agência"]}
+                Número da Conta : {conta_corrente[chave]["numero_conta"]}
+                CPF : {chave}
+                Usuário : {conta_corrente[chave]["usuario"]}
+""")  
+    
+    elif opcao_menu == "q":
         break
 
     else:
         print("operação inválida, por favor selecione novamente a operação desejada!")
+
+
+
+
+
+
